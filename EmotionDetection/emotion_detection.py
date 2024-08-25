b0e9129bf987bedf73f605018c13d9e0165e1531
Chythanya <chythanya.v27@gmail.com>
2024-08-25 10:38:45 -0400

    application testing

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,13 @@
+import requests, json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json = myobj, headers=headers)
+    formatted_text= json.loads(response.text)
+    emotion = formatted_text["emotionPredictions"][0]["emotion"]
+    sorted_emotion = sorted(emotion.items(), key=lambda x:x[1], reverse=True)
+    final = dict(sorted_emotion)
+    emotion['dominant_emotion'] = list(final.keys())[0]
+    return emotion
